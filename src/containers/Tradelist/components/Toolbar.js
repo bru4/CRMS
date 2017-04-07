883f1e94d66069b3acc41dd81522ce179be90aa9
kuangshu <252019869@qq.com>
2017-04-07 13:40:35 +0800

    完成组件结构

--- src/containers/Tradelist/components/Toolbar.js
+++ src/containers/Tradelist/components/Toolbar.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Row, Col, Button, Select, Input } from 'antd';
 
 const Toolbar = (props) => {
     return(
-        <div>
-            Toolbar
-        </div>
+        <Row className='toolbar'>
+            <Col span={1} offset={2}>手机号：</Col>
+            <Col span={3}><Input /></Col>
+            <Col span={2} offset={1}>
+                <Button>搜索</Button>
+            </Col>
+            <Col span={3} offset={8}>
+                <Select style={{ width: '90%' }}>
+                    <Select.Option value='推送成功'>推送成功</Select.Option>
+                    <Select.Option value='推送失败'>推送失败</Select.Option>
+                    <Select.Option value='全部推送'>全部推送</Select.Option>
+                </Select>
+            </Col>
+            <Col span={2}>
+                <Button>确定</Button>
+            </Col>
+        </Row>
     )
 }
 
--- src/containers/Tradelist/components/Tradelist.js
+++ src/containers/Tradelist/components/Tradelist.js
@@ -1,10 +1,72 @@
 import React from 'react'
+import { Table } from 'antd'
 
 const Tradelist = (props) => {
+    const columns = [
+        {
+            title: '序号',
+            key: 'idx',
+            width: 50,
+            render: (text, record, index) => index + 1,
+        }, {
+            title: '姓名',
+            width: 70,
+            key: 'truename',
+            dataIndex: 'truename',
+        }, {
+            title: '手机号',
+            width: 70,
+            key: 'restaurantname',
+            dataIndex: 'restaurantname',
+        }, {
+            title: '收货人姓名',
+            width: 100,
+            key: 'receivername',
+            dataIndex: 'receivername',
+        }, {
+            title: '收货地址',
+            width: 250,
+            key: 'receiverdistrict',
+            dataIndex: 'receiverdistrict',
+        }, {
+            title: '收货电话',
+            width: 100,
+            key: 'receivermobile',
+            dataIndex: 'receivermobile',
+        }, {
+            title: '试用产品',
+            width: 150,
+            key: 'reason',
+            dataIndex: 'reason',
+        }, {
+            title: '推送状态',
+            key: 'source',
+            width: 70,
+            dataIndex: 'source',
+            render: (text) => text === 0 ? '微信内' : '微信外',
+        }, {
+            title: '快递方式',
+            key: 'state',
+            width: 70,
+            dataIndex: 'state',
+
+        }, {
+            title: '物流单号',
+            key: 'status',
+            dataIndex: 'status',
+            width: 80,
+        }, {
+            title: '操作',
+            key: 'operation',
+            width: 120,
+            render: (text, record, index) => {
+            },
+        }
+    ];
     return(
-        <div>
-            Tradelist
-        </div>
+        <Table
+            columns={columns}
+        />
     )
 }
 
